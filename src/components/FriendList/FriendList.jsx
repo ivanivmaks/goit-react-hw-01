54a1708a8dbe65df44483f18a946f159e425788a
ivaniv_maks <ivanivmaks0@gmail.com>
2024-04-29 16:34:37 +0300

    update

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,8 +1,7 @@
-import FriendListItem from "./FriendListItem";
-import css from'./FriendList.module.css'
+import FriendListItem from "../FriendListItem/FriendListItem";
+import css from "./FriendList.module.css";
 
-
-export default function FriendList( {friends} ) {
+export default function FriendList({ friends }) {
   return (
     <ul className={css.friendList}>
       {friends.map((friend) => {
